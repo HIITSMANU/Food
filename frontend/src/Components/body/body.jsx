a04416d5f2cbaed8b62e3b186942de81c0667253
Sriya Saloni Singh <138764260+Sriya-Saloni-Singh@users.noreply.github.com>
2024-05-13 17:05:48 +0530

    commit

---
+++ frontend/src/Components/body/body.jsx
@@ -0,0 +1,13 @@
+// to add landing page here or anything
+import React from "react";
+import './body.css';
+
+const Body = ()=>{
+    return {
+         <div className = "body.div">
+        
+        </div>
+    }
+}
+
+export default Body;
